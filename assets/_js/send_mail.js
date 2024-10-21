fae0990cd99613bc04da41c3e27cba52eac0f87a
Jill <jho3292@gmail.com>
2024-10-21 08:50:20 -0400

    Update contact forms to not use formspree.

--- assets/_js/send_mail.js
+++ assets/_js/send_mail.js
@@ -1,4 +1,3 @@
-
 window.addEventListener("DOMContentLoaded", function() {
   // get the form elements defined in your form HTML above
   
@@ -50,6 +49,7 @@ window.addEventListener("DOMContentLoaded", function() {
   // helper function for sending an AJAX request
 
   function ajax(method, url, data, success, error) {
+    console.log(data);
     var xhr = new XMLHttpRequest();
     xhr.open(method, url);
     xhr.setRequestHeader("Accept", "application/json");
@@ -58,6 +58,7 @@ window.addEventListener("DOMContentLoaded", function() {
       if (xhr.status === 200) {
         success(xhr.response, xhr.responseType);
       } else {
+        console.log(error);
         error(xhr.status, xhr.response, xhr.responseType);
       }
     };
